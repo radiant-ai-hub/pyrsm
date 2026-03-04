2f9701dccccca2f329042fa15b4ad12d0baac1ad
Vincent Nijs <vnijs@ucsd.edu>
2026-03-03 22:07:42 -0800

    2.4.0

--- pyrsm/__init__.py
+++ pyrsm/__init__.py
@@ -2,7 +2,7 @@ from importlib import import_module
 
 import polars as pl
 
-__version__ = "2.3.2"
+__version__ = "2.4.0"
 
 # Global Polars config: always print full tables (no row/column truncation)
 pl.Config.set_tbl_rows(-1)
